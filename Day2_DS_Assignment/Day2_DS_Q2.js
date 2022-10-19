ae684827abaf568feb58aa41bc00a748adf5b903
Bhuvaneshwari Shirhattimath <114942437+BhuvaneshwariShirhattimath@users.noreply.github.com>
2022-10-19 23:11:55 +0530

    Add files via upload

---
+++ Day2_DS_Assignment/Day2_DS_Q2.js
@@ -0,0 +1,19 @@
+//Implement insertion sort algorithm to sort a numeric array in JavaScript.
+const insertionSort = arr =>
+  arr.reduce((acc, x) => {
+    if (!acc.length) return [x];
+    acc.some((y, j) => {
+      if (x <= y) {
+        acc.splice(j, 0, x);
+        return true;
+      }
+      if (x > y && j === acc.length - 1) {
+        acc.splice(j + 1, 0, x);
+        return true;
+      }
+      return false;
+    });
+    return acc;
+  }, []);
+ 
+console.log(insertionSort([6, 3, 4, 1]));
